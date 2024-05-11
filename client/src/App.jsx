838bf52f3b6af0701f0c8337ce77f3dc54fb5333
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-05-12 03:56:07 +0530

    Header File

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import AboutPage from './Components/Pages/AboutPage';
 import ProfilePage from './Components/Pages/ProfilePage';
 import SignIn from './Components/Pages/SignIn';
 import SignUp from './Components/Pages/SignUp';
+import Header from './Components/Header/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/about' element={<AboutPage/>}/>
