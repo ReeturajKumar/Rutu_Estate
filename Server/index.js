63a2dce737daeab8034158e186a00ca7852e7954
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-05-12 05:23:49 +0530

    create and run server

---
+++ Server/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(808, () => {
+  console.log('Server is running on port 8080')
+})
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -10,13 +10,13 @@ export default function Header() {
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Rutu</span>
             <span className='text-slate-700'>Estate</span>
-            </h1>
+          </h1>
         </Link>
-      <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-        <FaSearch className='text-slate-600' />
-      </form>
-      <ul className='flex gap-4'>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+          <FaSearch className='text-slate-600' />
+        </form>
+        <ul className='flex gap-4'>
         <Link to='/'><li className='hidden sm:inline text-slate-700'>Home</li></Link>
         <Link to='/about'><li className='hidden sm:inline text-slate-700'>About</li></Link>
         <Link to='/sign-in'><li className='text-slate-700'>Sign in</li></Link>
