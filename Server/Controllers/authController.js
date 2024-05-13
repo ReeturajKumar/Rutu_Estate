620efdf290de401a1f6bdb223724dd90af81957d
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-05-13 10:46:30 +0530

    Creating MIddleware

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -1,15 +1,16 @@
+import { errorHandler } from '../utils/error.js';
 import User from './../Models/userModel.js';
 import bcryptjs from 'bcryptjs'
 
 
-export const signup = async (req,res) => {
+export const signup = async (req,res, next) => {
   const { username, email, password} = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User ({username, email, password: hashedPassword});
   try {
     await newUser.save()
-res.status(201).json({message: "User Created Successfully "});
+    res.status(201).json({message: "User Created Successfully "});
   } catch (error) {
-    res.status(500).json(error.message)
+    next(error);
   }
 }
