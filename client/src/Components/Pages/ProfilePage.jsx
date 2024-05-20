b2ff2bfabf30443b1d6383362ef3b9396a2abc28
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-05-21 04:30:35 +0530

    final

--- client/src/Components/Pages/ProfilePage.jsx
+++ client/src/Components/Pages/ProfilePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import '../CSS/Profile.css';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import {app} from '../../firebase';
+import {app} from '../../firebase.js';
 
 import { updateUserStart, UpdateuserSuccess, UpdateuserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, logoutUserStart, logoutUserFailure, logoutSuccess } from '../../redux/userSlice';
 import { Link } from 'react-router-dom';
