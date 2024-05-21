29bc29770fe56e6c2cac1c1c661db7f230a399eb
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-05-21 15:11:45 +0530

    port

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(8080, () => {
-  console.log('Server is running on port 3000!');
+  console.log('Server is running on port 8080!');
 });
 
 app.use('/api/user', userRouter);
