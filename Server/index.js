d8a3abe738eed775fc747e22f5532931c913565b
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-05-21 09:48:01 +0530

    index

--- Server/index.js
+++ Server/index.js
@@ -3,52 +3,48 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './Routes/userRoute.js';
 import authRouter from './Routes/authRoute.js';
-import listingRouter from './Routes/listingRoute.js'
+import listingRouter from './Routes/listingRoute.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log('Connected to DB')})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
   .catch((err) => {
-    console.log(err)});
+    console.log(err);
+  });
 
-    const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(8080, () => {
-  console.log('Server is running on port 8080')});
-
-
-
-
-
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/listing' , listingRouter);
-
+app.use('/api/listing', listingRouter);
 
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
-
-
-
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success : false,
+    success: false,
     statusCode,
     message,
   });
